b8986d2bb99bb13622695bb6a9483b5625e1ed2d
JK Gunnink <jgunnink@gmail.com>
2017-03-01 22:38:53 +0800

    Initial commit. Custom 404 page and import bootstrap

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+if [ $# -lt 1 ]
+	then
+		echo 'No commit message given'
+		exit
+fi
+MSG=$1
+git checkout develop
+rm -rf public
+hugo
+cp CNAME ./public/CNAME
+git add .
+git commit -m "$MSG"
+git push origin develop && git subtree push --prefix public origin master
