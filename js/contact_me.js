fcb8c12d2878c3a1c04212f5ac24b3ebfade33de
JK Gunnink <jgunnink@gmail.com>
2017-03-08 14:16:09 +0800

    minor typo

--- js/contact_me.js
+++ js/contact_me.js
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thanks, " + firstName + ", your details have been registered!</strong>");
+                        .append("<strong>Thanks " + firstName + ", your details have been registered!</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
